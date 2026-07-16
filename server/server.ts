ed8e5e61baa50944c5e12305ac08f6be91c0e56f
mayur-sh1 <mayursharma20000@gmail.com>
2026-07-16 06:37:29 +0530

    fixing cors links

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ declare module 'express-session' {
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000',"https://thumblify-client-eight-gamma.vercel.app/"],
+    origin: ['http://localhost:5173', 'http://localhost:3000',"https://thumblify-client-eight-gamma.vercel.app"],
     credentials: true
 }))
 app.use(session({
